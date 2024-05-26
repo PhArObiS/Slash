477829fc0414c3d14799d010587c800ac2ca8e07
Neil Lopes <127422402+PhArObiS@users.noreply.github.com>
2024-05-26 13:20:52 -0400

    Treasure Class
    Added a treasure glass and a BP for it.  Added sounds when picked up and a mesh

---
+++ Source/Slash/Private/Treasure.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Treasure.h"
+#include "SlashCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
+void ATreasure::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ASlashCharacter* SlashCharcter = Cast<ASlashCharacter>(OtherActor);
+	if (SlashCharcter)
+	{
+		if (PickupSound)
+		{
+			UGameplayStatics::PlaySoundAtLocation(
+				this,
+				PickupSound,
+				GetActorLocation()
+				);
+		}
+		Destroy();
+	}
+}
