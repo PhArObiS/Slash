6c51fed7baadec1224dda141e223c1168a771f87
Neil Lopes <127422402+PhArObiS@users.noreply.github.com>
2024-06-05 19:08:49 -0400

    Slash Overlay
    Added a overlay for health and stamina.  Added coins and souls.  Create a class with variables.

---
+++ Source/Slash/Private/HUD/SlashOverlay.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HUD/SlashOverlay.h"
+#include "Components/ProgressBar.h"
+
+void USlashOverlay::SetHealthBarPercent(float Percent)
+{
+	if (HealthProgressBar)
+	{
+		HealthProgressBar->SetPercent(Percent);
+	}
+}
+
+void USlashOverlay::SetStaminaBarPercent(float Percent)
+{
+	if (StaminaProgressBar)
+	{
+		StaminaProgressBar->SetPercent(Percent);
+	}
+}
